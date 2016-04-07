c925499d8e8220b7d2439de8836ccf253dea0a84
narendramukherjee <narendra.mukherjee@gmail.com>
2016-04-07 11:42:20 -0400

    Cleaned up HMM code to use pomegranate

--- blech_hmm.py
+++ blech_hmm.py
@@ -3,11 +3,11 @@ from pomegranate import *
 import numpy as np
 import multiprocessing as mp
 
-def poisson_hmm_implement(n_states, threshold, seeds, n_cpu, spikes, off_trials, edge_inertia, dist_inertia):
+def poisson_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia):
 
 	# Create a pool of asynchronous n_cpu processes running poisson_hmm() - no. of processes equal to seeds
 	pool = mp.Pool(processes = n_cpu)
-	results = [pool.apply_async(poisson_hmm, args = (n_states, threshold, spikes, seed, off_trials, edge_inertia, dist_inertia,)) for seed in range(seeds)]
+	results = [pool.apply_async(poisson_hmm, args = (n_states, threshold, binned_spikes, seed, off_trials, edge_inertia, dist_inertia,)) for seed in range(seeds)]
 	output = [p.get() for p in results]
 
 	# Find the process that ended up with the highest log likelihood, and return it as the solution. If several processes ended up with the highest log likelihood, just pick the earliest one
@@ -15,11 +15,11 @@ def poisson_hmm_implement(n_states, threshold, seeds, n_cpu, spikes, off_trials,
 	maximum_pos = np.where(log_probs == np.max(log_probs))[0][0]
 	return output[maximum_pos]	
 
-def multinomial_hmm_implement(n_states, threshold, seeds, n_cpu, spikes, off_trials, edge_inertia, dist_inertia):
+def multinomial_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia):
 
 	# Create a pool of asynchronous n_cpu processes running multinomial_hmm() - no. of processes equal to seeds
 	pool = mp.Pool(processes = n_cpu)
-	results = [pool.apply_async(multinomial_hmm, args = (n_states, threshold, spikes, seed, off_trials, edge_inertia, dist_inertia,)) for seed in range(seeds)]
+	results = [pool.apply_async(multinomial_hmm, args = (n_states, threshold, binned_spikes, seed, off_trials, edge_inertia, dist_inertia,)) for seed in range(seeds)]
 	output = [p.get() for p in results]
 
 	# Find the process that ended up with the highest log likelihood, and return it as the solution. If several processes ended up with the highest log likelihood, just pick the earliest one
@@ -27,7 +27,7 @@ def multinomial_hmm_implement(n_states, threshold, seeds, n_cpu, spikes, off_tri
 	maximum_pos = np.where(log_probs == np.max(log_probs))[0][0]
 	return output[maximum_pos]		
 
-def poisson_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia, dist_inertia):
+def poisson_hmm(n_states, threshold, binned_spikes, seed, off_trials, edge_inertia, dist_inertia):
 
 	# Make a pomegranate HiddenMarkovModel object
 	model = HiddenMarkovModel('%i' % seed) 
@@ -36,7 +36,7 @@ def poisson_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia, dis
 	for i in range(n_states):
 		#emission_slice = (int((float(i)/n_states)*spikes.shape[1]), int((float(i+1)/n_states)*spikes.shape[1]))
 		#initial_emissions = np.mean(spikes[off_trials, emission_slice[0]:emission_slice[1], :], axis = (0, 1))*(np.random.random())
-		states.append(State(IndependentComponentsDistribution([PoissonDistribution(np.random.rand()) for unit in range(spikes.shape[2])]), name = 'State%i' % (i+1)))
+		states.append(State(IndependentComponentsDistribution([PoissonDistribution(np.random.rand()) for unit in range(binned_spikes.shape[2])]), name = 'State%i' % (i+1)))
 		
 	model.add_states(states)
 	# Add transitions from model.start to each state (equal probabilties)
@@ -56,36 +56,36 @@ def poisson_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia, dis
 	model.bake()
 
 	# Train the model only on the trials indicated by off_trials
-	model.fit(spikes[off_trials, :, :], algorithm = 'baum-welch', stop_threshold = threshold, edge_inertia = edge_inertia, distribution_inertia = dist_inertia, verbose = False)
-	log_prob = [model.log_probability(spikes[i, :, :]) for i in off_trials]
+	model.fit(binned_spikes[off_trials, :, :], algorithm = 'baum-welch', stop_threshold = threshold, edge_inertia = edge_inertia, distribution_inertia = dist_inertia, verbose = False)
+	log_prob = [model.log_probability(binned_spikes[i, :, :]) for i in off_trials]
 	log_prob = np.sum(log_prob)
 
 	# Set up things to return the parameters of the model - the state emission and transition matrix 
 	state_emissions = []
 	state_transitions = np.exp(model.dense_transition_matrix())
 	for i in range(n_states):
-		state_emissions.append([model.states[i].distribution.parameters[0][j].parameters[0] for j in range(spikes.shape[2])])
+		state_emissions.append([model.states[i].distribution.parameters[0][j].parameters[0] for j in range(binned_spikes.shape[2])])
 	state_emissions = np.array(state_emissions)
 
 	# Get the posterior probability sequence to return
 	posterior_proba = []
-	for i in range(spikes.shape[0]):
-		c, d = model.forward_backward(spikes[i, :, :])
+	for i in range(binned_spikes.shape[0]):
+		c, d = model.forward_backward(binned_spikes[i, :, :])
 		posterior_proba.append(d)
 	posterior_proba = np.exp(np.array(posterior_proba))
 
 	# Get the json representation of the model - will be needed if we need to reload the model anytime
 	model_json = model.to_json()
 
-	return model_json, log_prob, 2*((n_states)**2 + n_states*spikes.shape[2]) - 2*log_prob, (np.log(len(off_trials)*spikes.shape[1]))*((n_states)**2 + n_states*spikes.shape[2]) - 2*log_prob, state_emissions, state_transitions, posterior_proba
+	return model_json, log_prob, 2*((n_states)**2 + n_states*binned_spikes.shape[2]) - 2*log_prob, (np.log(len(off_trials)*binned_spikes.shape[1]))*((n_states)**2 + n_states*binned_spikes.shape[2]) - 2*log_prob, state_emissions, state_transitions, posterior_proba
 	
-def multinomial_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia, dist_inertia):
+def multinomial_hmm(n_states, threshold, binned_spikes, seed, off_trials, edge_inertia, dist_inertia):
 
 	# Make a pomegranate HiddenMarkovModel object
 	model = HiddenMarkovModel('%i' % seed) 
 	states = []
 	# Make a pomegranate Discrete distribution object with emissions = range(n_units + 1) - 1 for each state
-	n_units = int(np.max(spikes))
+	n_units = int(np.max(binned_spikes))
 	for i in range(n_states):
 		dist_dict = {}
 		prob_list = np.random.random(n_units + 1)
@@ -112,8 +112,8 @@ def multinomial_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia,
 	model.bake()
 
 	# Train the model only on the trials indicated by off_trials
-	model.train(spikes[off_trials, :], algorithm = 'baum-welch', stop_threshold = threshold, edge_inertia = edge_inertia, distribution_inertia = dist_inertia)
-	log_prob = [model.log_probability(spikes[i, :]) for i in off_trials]
+	model.fit(binned_spikes[off_trials, :], algorithm = 'baum-welch', stop_threshold = threshold, edge_inertia = edge_inertia, distribution_inertia = dist_inertia, verbose = False)
+	log_prob = [model.log_probability(binned_spikes[i, :]) for i in off_trials]
 	log_prob = np.sum(log_prob)
 
 	# Set up things to return the parameters of the model - the state emission dicts and transition matrix 
@@ -123,12 +123,12 @@ def multinomial_hmm(n_states, threshold, spikes, seed, off_trials, edge_inertia,
 		state_emissions.append(model.states[i].distribution.parameters[0])
 	
 	# Get the posterior probability sequence to return
-	posterior_proba = np.zeros((spikes.shape[0], spikes.shape[1], n_states))
-	for i in range(spikes.shape[0]):
-		c, d = model.forward_backward(spikes[i, :])
+	posterior_proba = np.zeros((binned_spikes.shape[0], binned_spikes.shape[1], n_states))
+	for i in range(binned_spikes.shape[0]):
+		c, d = model.forward_backward(binned_spikes[i, :])
 		posterior_proba[i, :, :] = np.exp(d)
 
 	# Get the json representation of the model - will be needed if we need to reload the model anytime
 	model_json = model.to_json()
 	
-	return model_json, log_prob, 2*((n_states)**2 + n_states*(n_units + 1)) - 2*log_prob, (np.log(len(off_trials)*spikes.shape[1]))*((n_states)**2 + n_states*(n_units + 1)) - 2*log_prob, state_emissions, state_transitions, posterior_proba	
+	return model_json, log_prob, 2*((n_states)**2 + n_states*(n_units + 1)) - 2*log_prob, (np.log(len(off_trials)*binned_spikes.shape[1]))*((n_states)**2 + n_states*(n_units + 1)) - 2*log_prob, state_emissions, state_transitions, posterior_proba	
