97fb064cda41e0c4be251e5da5fe39e4518a105e
narendramukherjee <narendra.mukherjee@gmail.com>
2016-04-24 19:14:20 -0400

    Numba optimized function to calculate the pairwise distances (actually similarities) between units

--- blech_units_distance.py
+++ blech_units_distance.py
@@ -4,7 +4,19 @@ import tables
 import easygui
 import sys
 import os
-import matplotlib.pyplot as plt
+from numba import jit
+
+# Numba compiled function to compute the number of spikes in this_unit_times that are within 1 ms of a spike in other_unit_times, and vice versa
+@jit(nogil = True)
+def unit_distance(this_unit_times, other_unit_times):
+	this_unit_counter = 0
+	other_unit_counter = 0
+	for i in range(len(this_unit_times)):
+		for j in range(len(other_unit_times)):
+			if np.abs(this_unit_times[i] - other_unit_times[j]) <= 1.0:
+				this_unit_counter += 1
+				other_unit_counter += 1
+	return this_unit_counter, other_unit_counter
 
 # Ask for the directory where the hdf5 file sits, and change to that directory
 dir_name = easygui.diropenbox()
@@ -25,7 +37,26 @@ units = hf5.list_nodes('/sorted_units')
 
 # Now go through the units one by one, and get the pairwise distances between them
 # Distance is defined as the percentage of spikes of the reference unit that have a spike from the compared unit within 1 ms
+print "=================="
+print "Distance calculation starting"
 unit_distances = np.zeros((len(units), len(units)))
+for this_unit in range(len(units)):
+	this_unit_times = (units[this_unit].times[:])/30.0
+	for other_unit in range(len(units)): 
+		if other_unit < this_unit:
+			continue
+		other_unit_times = (units[other_unit].times[:])/30.0
+		this_unit_counter, other_unit_counter = unit_distance(this_unit_times, other_unit_times)
+		unit_distances[this_unit, other_unit] = 100.0*(float(this_unit_counter)/len(this_unit_times))
+		unit_distances[other_unit, this_unit] = 100.0*(float(other_unit_counter)/len(other_unit_times))
+	# Print the progress to the window
+	print "Unit %i of %i completed" % (this_unit+1, len(units))
+print "Distance calculation complete, results being saved to file"
+print "=================="
+
+
+'''
+Tried to do everything in the numba compiled loop through vectorized numpy code - failed because of memory errors involved in broadcasting arrays
 for this_unit in range(len(units)):
 	this_unit_times = units[this_unit].times[:]
 	for other_unit in range(len(units)): 
@@ -38,7 +69,7 @@ for this_unit in range(len(units)):
 		diff_this_other = np.min(diff, axis = 1)/30.0
 		diff_other_this = np.min(diff, axis = 0)/30.0
 		unit_distances[this_unit, other_unit] = 100.0*len(np.where(diff_this_other <= 1.0)[0])/len(diff_this_other)
-		unit_distances[other_unit, this_unit] = 100.0*len(np.where(diff_other_this <= 1.0)[0])/len(diff_other_this)
+		unit_distances[other_unit, this_unit] = 100.0*len(np.where(diff_other_this <= 1.0)[0])/len(diff_other_this)'''
 	
 # Make a node for storing unit distances under /sorted_units. First try to delete it, and pass if it exists
 try:
