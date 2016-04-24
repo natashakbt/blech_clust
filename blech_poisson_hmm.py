c1adf9231ee3801b3846aa3316e6edb38b052468
narendramukherjee <narendra.mukherjee@gmail.com>
2016-04-23 23:13:18 -0400

    2 new features: 1.) Units can be chosen before running HMM, 2.) Feedforward transition models can be used

--- blech_poisson_hmm.py
+++ blech_poisson_hmm.py
@@ -21,15 +21,18 @@ os.chdir(dir_name[0][:-1])
 # Pull out the NSLOTS - number of CPUs allotted
 n_cpu = int(os.getenv('NSLOTS'))
 
-# Get the names of all files in the current directory, and find the .params and hdf5 (.h5) file
+# Get the names of all files in the current directory, and find the .hmm_params, .hmm_units and hdf5 (.h5) file
 file_list = os.listdir('./')
 hdf5_name = ''
 params_file = ''
+units_file = ''
 for files in file_list:
 	if files[-2:] == 'h5':
 		hdf5_name = files
 	if files[-10:] == 'hmm_params':
 		params_file = files
+	if files[-9:] == 'hmm_units':
+		units_file = files
 
 # Read the .hmm_params file
 f = open(params_file, 'r')
@@ -50,6 +53,14 @@ pre_stim = int(params[7])
 bin_size = int(params[8])
 pre_stim_hmm = int(params[9])
 post_stim_hmm = int(params[10])
+hmm_type = params[11][:-1]
+
+# Read the chosen units
+f = open(units_file, 'r')
+chosen_units = []
+for line in f.readlines():
+	chosen_units.append(int(line))
+chosen_units = np.array(chosen_units)
 
 # Open up hdf5 file
 hf5 = tables.open_file(hdf5_name, 'r+')
@@ -57,8 +68,8 @@ hf5 = tables.open_file(hdf5_name, 'r+')
 # Get the spike array from the required taste/input
 exec('spikes = hf5.root.spike_trains.dig_in_%i.spike_array[:]' % taste)
 
-# Slice out the required portion of the spike array, and bin it
-spikes = spikes[:, :, pre_stim - pre_stim_hmm:pre_stim + post_stim_hmm]
+# Slice out the required portion of the spike array, and bin it (only use the units in chosen_units)
+spikes = spikes[:, chosen_units, pre_stim - pre_stim_hmm:pre_stim + post_stim_hmm]
 binned_spikes = np.zeros((spikes.shape[0], spikes.shape[1], (pre_stim_hmm + post_stim_hmm)/bin_size))
 time = []
 for i in range(spikes.shape[0]):
@@ -75,50 +86,50 @@ off_trials = np.arange(binned_spikes.shape[0])
 for n_states in range(min_states, max_states + 1):
 	# Run the Poisson HMM - skip if it doesn't converge for some reason
 	try:
-		result = poisson_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia)
+		result = poisson_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia, hmm_type)
 		hmm_results.append((n_states, result))
 	except: 
 		continue
 
 # Delete the poisson_hmm_results node under /spike_trains/dig_in_(taste)/ if it exists
 try:
-	hf5.remove_node('/spike_trains/dig_in_%i/poisson_hmm_results' % taste, recursive = True)
+	hf5.remove_node('/spike_trains/dig_in_%i/%s_poisson_hmm_results' % (taste, hmm_type), recursive = True)
 except:
 	pass
 
 # Then create the poisson_hmm_results group
-hf5.create_group('/spike_trains/dig_in_%i/' % taste, 'poisson_hmm_results')
+hf5.create_group('/spike_trains/dig_in_%i/' % taste, '%s_poisson_hmm_results' % hmm_type)
 hf5.flush()
 
 # Delete the Poisson folder within HMM_plots if it exists for this taste
 try:
-	os.system("rm -r ./HMM_plots/dig_in_%i/Poisson" % taste)
+	os.system("rm -r ./HMM_plots/dig_in_%i/%s_Poisson" % (taste, hmm_type))
 except:
 	pass	
 
-# Make a folder for plots of Multinomial HMM analysis
-os.mkdir("HMM_plots/dig_in_%i/Poisson" % taste)
+# Make a folder for plots of Poisson HMM analysis
+os.mkdir("HMM_plots/dig_in_%i/%s_Poisson" % (taste, hmm_type))
 
 # Go through the HMM results, and make plots for each state and each trial
 for result in hmm_results:
 	# Make a directory for this number of states
-	os.mkdir("HMM_plots/dig_in_%i/Poisson/states_%i" % (taste, result[0]))
+	os.mkdir("HMM_plots/dig_in_%i/%s_Poisson/states_%i" % (taste, hmm_type, result[0]))
 		
 	# Make a group under poisson_hmm_results for this number of states
-	hf5.create_group('/spike_trains/dig_in_%i/poisson_hmm_results' % taste, 'states_%i' % (result[0])) 
+	hf5.create_group('/spike_trains/dig_in_%i/%s_poisson_hmm_results' % (taste, hmm_type), 'states_%i' % (result[0])) 
 	# Write the emission and transition probabilties to this group
-	emission_probs = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'emission_probs', result[1][4])
-	transition_probs = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'transition_probs', result[1][5])
-	posterior_proba = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'posterior_proba', result[1][6])
+	emission_probs = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'emission_probs', result[1][4])
+	transition_probs = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'transition_probs', result[1][5])
+	posterior_proba = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'posterior_proba', result[1][6])
 
 	# Also write the json model string to file
-	model_json = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'model_json', result[1][0])
+	model_json = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'model_json', result[1][0])
 
 	# Write the log-likelihood, AIC/BIC score, and time vector to the hdf5 file too
-	log_prob = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'log_likelihood', np.array(result[1][1]))
-	aic = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'aic', np.array(result[1][2]))
-	bic = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'bic', np.array(result[1][3]))
-	time_vect = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/states_%i' % (taste, result[0]), 'time', time)
+	log_prob = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'log_likelihood', np.array(result[1][1]))
+	aic = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'aic', np.array(result[1][2]))
+	bic = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'bic', np.array(result[1][3]))
+	time_vect = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/states_%i' % (taste, hmm_type, result[0]), 'time', time)
 	hf5.flush()
 
 	# Go through the trials in binned_spikes and plot the trial-wise posterior probabilities with the unit rasters
@@ -142,7 +153,7 @@ for result in hmm_results:
 		plt.xlabel('Time post stimulus (ms)')
 		plt.ylabel('Probability of HMM states' + '\n' + 'Unit number')
 		plt.title('Trial %i' % (i+1) + '\n' + 'RSU: red, FS: blue, Multi: black')
-		fig.savefig('HMM_plots/dig_in_%i/Poisson/states_%i/Trial_%i.png' % (taste, result[0], (i+1)))
+		fig.savefig('HMM_plots/dig_in_%i/%s_Poisson/states_%i/Trial_%i.png' % (taste, hmm_type, result[0], (i+1)))
 		plt.close("all")
 
 # Check if the laser_array exists - if it does, perform a 2nd round of HMM training on just the non-laser trials
@@ -160,51 +171,51 @@ if len(laser_exists) > 0:
 	for n_states in range(min_states, max_states + 1):
 		# Run the Poisson HMM - skip it if it doesn't converge
 		try:
-			result = poisson_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia)
+			result = poisson_hmm_implement(n_states, threshold, seeds, n_cpu, binned_spikes, off_trials, edge_inertia, dist_inertia, hmm_type)
 			hmm_results.append((n_states, result))
 		except:
 			continue
 
 	# Delete the laser node under /spike_trains/dig_in_(taste)/poisson_hmm_results/ if it exists
 	try:
-		exec("hf5.remove_node('/spike_trains/dig_in_%i/poisson_hmm_results/laser' % taste, recursive = True)")
+		exec("hf5.remove_node('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser' % (taste, hmm_type), recursive = True)")
 	except:
 		pass
 
 	# Then create the poisson_hmm_results group
-	exec("hf5.create_group('/spike_trains/dig_in_%i/poisson_hmm_results' % taste, 'laser')")
+	exec("hf5.create_group('/spike_trains/dig_in_%i/%s_poisson_hmm_results' % (taste, hmm_type), 'laser')")
 	hf5.flush()
 
 	# Delete the laser folder within HMM_plots/Poisson if it exists for this taste
 	try:
-		os.system("rm -r ./HMM_plots/dig_in_%i/Poisson/laser" % taste)
+		os.system("rm -r ./HMM_plots/dig_in_%i/%s_Poisson/laser" % (taste, hmm_type))
 	except:
 		pass	
 
 	# Make a folder for plots of Poisson HMM analysis
-	os.mkdir("HMM_plots/dig_in_%i/Poisson/laser" % taste)
+	os.mkdir("HMM_plots/dig_in_%i/%s_Poisson/laser" % (taste, hmm_type))
 	
 	# Go through the HMM results, and make plots for each state and each trial
 	for result in hmm_results:
 		
 		# Make a directory for this number of states
-		os.mkdir("HMM_plots/dig_in_%i/Poisson/laser/states_%i" % (taste, result[0]))
+		os.mkdir("HMM_plots/dig_in_%i/%s_Poisson/laser/states_%i" % (taste, hmm_type, result[0]))
 		
 		# Make a group under poisson_hmm_results for this number of states
-		hf5.create_group('/spike_trains/dig_in_%i/poisson_hmm_results/laser' % taste, 'states_%i' % (result[0])) 
+		hf5.create_group('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser' % (taste, hmm_type), 'states_%i' % (result[0])) 
 		# Write the emission and transition probabilties to this group
-		emission_probs = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'emission_probs', result[1][4])
-		transition_probs = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'transition_probs', result[1][5])
-		posterior_proba = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'posterior_proba', result[1][6])
+		emission_probs = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'emission_probs', result[1][4])
+		transition_probs = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'transition_probs', result[1][5])
+		posterior_proba = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'posterior_proba', result[1][6])
 
 		# Also write the json model string to file
-		model_json = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'model_json', result[1][0])
+		model_json = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'model_json', result[1][0])
 
 		# Write the log-likelihood and AIC/BIC score to the hdf5 file too
-		log_prob = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'log_likelihood', np.array(result[1][1]))
-		aic = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'aic', np.array(result[1][2]))
-		bic = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'bic', np.array(result[1][3]))
-		time_vect = hf5.create_array('/spike_trains/dig_in_%i/poisson_hmm_results/laser/states_%i' % (taste, result[0]), 'time', time)
+		log_prob = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'log_likelihood', np.array(result[1][1]))
+		aic = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'aic', np.array(result[1][2]))
+		bic = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'bic', np.array(result[1][3]))
+		time_vect = hf5.create_array('/spike_trains/dig_in_%i/%s_poisson_hmm_results/laser/states_%i' % (taste, hmm_type, result[0]), 'time', time)
 		hf5.flush()
 
 		# Go through the trials in binned_spikes and plot the trial-wise posterior probabilities and raster plots
@@ -232,7 +243,7 @@ if len(laser_exists) > 0:
 			plt.xlabel('Time post stimulus (ms)')
 			plt.ylabel('Probability of HMM states' + '\n' + 'Unit number')
 			plt.title('Trial %i' % (i+1) + '\n' + 'RSU: red, FS: blue, Multi: black')
-			fig.savefig('HMM_plots/dig_in_%i/Poisson/laser/states_%i/%sTrial_%i.png' % (taste, result[0], label, (i+1)))
+			fig.savefig('HMM_plots/dig_in_%i/%s_Poisson/laser/states_%i/%sTrial_%i.png' % (taste, hmm_type, result[0], label, (i+1)))
 			plt.close("all")
 		
 hf5.close()	
--- blech_setup_hmm.py
+++ blech_setup_hmm.py
@@ -37,6 +37,32 @@ for i in range(len(tastes)):
 	if str(tastes[i]).split('/')[-1] in hmm_taste:
 		taste_num = i
 
+# Ask the user to choose the units to run the HMM on
+all_units = hf5.list_nodes('/sorted_units')
+all_units = np.array([int(str(unit).split('/')[-1][4:7]) for unit in all_units])
+single_units = np.array([i for i in range(len(all_units)) if hf5.root.unit_descriptor[i]["single_unit"] == 1]) + 1
+chosen_units = []
+units_choose = easygui.multchoicebox(msg = 'How do you want to choose units for the HMM?', choices = ('All units', 'Single units', 'Random choice over all units', 'Random choice over single units', 'Custom choice'))
+if units_choose[0] == 'All units':
+	chosen_units = all_units
+elif units_choose[0] == 'Single units':
+	chosen_units = single_units
+elif units_choose[0] == 'Random choice over all units':
+	num_units = easygui.multenterbox(msg = 'How many units do you want to choose?', fields = ['# of units (Total = %i)' % len(all_units)])
+	num_units = int(num_units[0])
+	chosen_units = np.random.choice(all_units, size = num_units, replace = True)
+elif units_choose[0] == 'Random choice over single units':
+	num_units = easygui.multenterbox(msg = 'How many single units do you want to choose?', fields = ['# of single units (Total = %i)' % len(single_units)])
+	num_units = int(num_units[0])
+	chosen_units = np.random.choice(single_units, size = num_units, replace = True)
+else:
+	chosen_units = easygui.multchoicebox(msg = 'Which units do you want to choose?', choices = ([i for i in all_units]))
+	for i in range(len(chosen_units)):
+		chosen_units[i] = int(chosen_units[i])
+
+# Convert the chosen units into numpy array, and subtract 1 - this gives us the unit number to use in the spike arrays (because they run from 0 to n-1)
+chosen_units = np.array(chosen_units) - 1
+
 # Create the folder for storing the plots coming in from HMM analysis of the data - pass if it exists
 try:
 	os.mkdir("HMM_plots")
@@ -50,6 +76,10 @@ except:
 # Ask the user for the parameters to process spike trains
 spike_params = easygui.multenterbox(msg = 'Fill in the parameters for processing your spike trains', fields = ['Pre-stimulus time used for making spike trains (ms)', 'Bin size for HMM (ms) - usually 10', 'Pre-stimulus time for HMM (ms)', 'Post-stimulus time for HMM (ms)'])
 
+# Ask the user to choose the type of HMM they want to fit - generic or feedforward 
+hmm_type = easygui.multchoicebox(msg = 'Which type of HMM do you want to fit?', choices = ('generic', 'feedforward'))
+hmm_type = hmm_type[0]
+
 # Print the paramaters to blech.hmm_params
 f = open('blech.hmm_params', 'w')
 for params in hmm_params:
@@ -57,6 +87,13 @@ for params in hmm_params:
 print>>f, taste_num
 for params in spike_params:
 	print>>f, params
+print>>f, hmm_type
+f.close()
+
+# Print the chosen units to blech.hmm_units
+f = open('blech.hmm_units', 'w')
+for unit in chosen_units:
+	print>>f, unit
 f.close()
 
 # Grab Brandeis unet username
