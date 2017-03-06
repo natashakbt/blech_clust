ded7c6baf06a53b663c582c673759da0a5d842c6
narendramukherjee <narendra.mukherjee@gmail.com>
2017-03-06 13:11:57 -0500

    Changed to 1.) Mention electrode number, 2.) Be consistent with unit numbering elsewhere

--- blech_units_plot.py
+++ blech_units_plot.py
@@ -38,8 +38,8 @@ for unit in range(len(units)):
 	plt.plot(x - 15, waveforms[:, ::10].T, linewidth = 0.01, color = 'red')
 	plt.xlabel('Time')
 	plt.ylabel('Voltage (microvolts)')
-	plt.title('Unit %i, total waveforms = %i' % (unit+1, waveforms.shape[0]) + '\n' + 'Single Unit: %i, RSU: %i, FS: %i' % (hf5.root.unit_descriptor[unit]['single_unit'], hf5.root.unit_descriptor[unit]['regular_spiking'], hf5.root.unit_descriptor[unit]['fast_spiking']))
-	fig.savefig('./unit_waveforms_plots/Unit%i.png' % (unit + 1))
+	plt.title('Unit %i, total waveforms = %i' % (unit, waveforms.shape[0]) + '\n' + 'Electrode: %i, Single Unit: %i, RSU: %i, FS: %i' % (hf5.root.unit_descriptor[unit]['electrode_number'], hf5.root.unit_descriptor[unit]['single_unit'], hf5.root.unit_descriptor[unit]['regular_spiking'], hf5.root.unit_descriptor[unit]['fast_spiking']))
+	fig.savefig('./unit_waveforms_plots/Unit%i.png' % (unit))
 	plt.close("all")
 
 hf5.close()
