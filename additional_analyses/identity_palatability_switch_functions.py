8e79308b350f158f2a8af57329a1efd396bc0071
narendramukherjee <narendra.mukherjee@gmail.com>
2017-04-22 12:30:45 -0400

    First commit of codes to run MCMC to get identity/palatability change points

---
+++ additional_analyses/identity_palatability_switch_functions.py
@@ -0,0 +1,164 @@
+# Import stuff!
+import numpy as np
+import tables
+import easygui
+import sys
+import os
+import pymc3 as pm
+import theano.tensor as tt
+
+def laser_off_trials(data, db):
+	
+	# Count the number of emissions in the dataset
+	num_emissions = len(np.unique(data))	
+
+	# Make the pymc3 model
+	with pm.Model() as model:
+		# Dirichlet prior on the emission/spiking probabilities - 4 states
+		p = pm.Dirichlet('p', np.ones(num_emissions), shape = (4, num_emissions))
+
+		# Discrete Uniform switch times
+		# Switch from detection to identity firing
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		# Switch from identity to palatability firing
+		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		# Switch from palatability firing to end
+		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 230)
+
+		# Add potentials to keep the switch times from coming too close to each other
+		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
+		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
+
+		# Get the actual state numbers based on the switch times
+		states1 = tt.switch(tt.and_(t1 <= np.arange(250), t2 >= np.arange(250)), 1, 0)
+		states2 = tt.switch(tt.and_(t2 <= np.arange(250), t3 >= np.arange(250)), 2, 0)
+		states3 = tt.switch(t3 <= np.arange(250), 0, 3)
+		states = states1 + states2 + states3
+
+		# Categorical observations
+		obs = pm.Categorical('obs', p = p[states], observed = data[:])
+
+	# Inference button :D
+	with model:
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, trace = db)
+
+	# Return the inference!
+	return model, tr
+
+def laser_early_trials(data, db):
+	
+	# Count the number of emissions in the dataset
+	num_emissions = len(np.unique(data))	
+
+	# Make the pymc3 model
+	with pm.Model() as model:
+		# Dirichlet prior on the emission/spiking probabilities - 4 states
+		p = pm.Dirichlet('p', np.ones(num_emissions), shape = (4, num_emissions))
+
+		# Discrete Uniform switch times
+		# Switch from detection to identity firing
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		# Switch from identity to palatability firing
+		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		# Switch from palatability firing to end
+		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 180)
+
+		# Add potentials to keep the switch times from coming too close to each other
+		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
+		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
+
+		# Get the actual state numbers based on the switch times
+		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
+		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
+		states3 = tt.switch(t3 <= np.arange(200), 0, 3)
+		states = states1 + states2 + states3
+
+		# Categorical observations
+		obs = pm.Categorical('obs', p = p[states], observed = data[50:])
+
+	# Inference button :D
+	with model:
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, trace = db)
+
+	# Return the inference!
+	return model, tr	
+
+def laser_middle_trials(data, db):
+	
+	# Count the number of emissions in the dataset
+	num_emissions = len(np.unique(data))	
+
+	# Make the pymc3 model
+	with pm.Model() as model:
+		# Dirichlet prior on the emission/spiking probabilities - 4 states
+		p = pm.Dirichlet('p', np.ones(num_emissions), shape = (4, num_emissions))
+
+		# Discrete Uniform switch times
+		# Switch from detection to identity firing
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		# Switch from identity to palatability firing
+		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 100)
+		# Switch from palatability firing to end
+		t3 = pm.DiscreteUniform('t3', lower = 90, upper = 180)
+
+		# Add potentials to keep the switch times from coming too close to each other
+		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
+		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
+
+		# Get the actual state numbers based on the switch times
+		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
+		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
+		states3 = tt.switch(t3 <= np.arange(200), 0, 3)
+		states = states1 + states2 + states3
+
+		# Categorical observations
+		obs = pm.Categorical('obs', p = p[states], observed = np.append(data[:70], data[120:]))
+
+	# Inference button :D
+	with model:
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 60, 't3': 110}, trace = db)
+
+	# Return the inference!
+	return model, tr	
+
+def laser_late_trials(data, db):
+	
+	# Count the number of emissions in the dataset
+	num_emissions = len(np.unique(data))	
+
+	# Make the pymc3 model
+	with pm.Model() as model:
+		# Dirichlet prior on the emission/spiking probabilities - 4 states
+		p = pm.Dirichlet('p', np.ones(num_emissions), shape = (4, num_emissions))
+
+		# Discrete Uniform switch times
+		# Switch from detection to identity firing
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		# Switch from identity to palatability firing
+		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		# Switch from palatability firing to end
+		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 180)
+
+		# Add potentials to keep the switch times from coming too close to each other
+		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
+		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
+
+		# Get the actual state numbers based on the switch times
+		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
+		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
+		states3 = tt.switch(t3 <= np.arange(200), 0, 3)
+		states = states1 + states2 + states3
+
+		# Categorical observations
+		obs = pm.Categorical('obs', p = p[states], observed = np.append(data[:140], data[190:]))
+
+	# Inference button :D
+	with model:
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, trace = db)
+
+	# Return the inference!
+	return model, tr	
---
+++ additional_analyses/identity_palatability_switch_process.py
@@ -0,0 +1,66 @@
+# Import stuff!
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import tables
+import easygui
+import sys
+import os
+import pymc3 as pm
+import theano.tensor as tt
+import identity_palatability_switch_functions as fn
+
+# Read the blech_MCMC.dir file and change to the data directory
+f = open('blech_MCMC.dir', 'r')
+dir_name = []
+for line in f.readlines():
+	dir_name.append(line)
+f.close()
+os.chdir(dir_name[0][:-1])
+
+# If running on jetstream (or personal computer) using GNU parallel, get sys.argv[1] - this is the trial number to be looked at
+trial_num = int(sys.argv[1]) - 1
+
+# Look for the hdf5 file in the directory
+file_list = os.listdir('./')
+hdf5_name = ''
+for files in file_list:
+	if files[-2:] == 'h5':
+		hdf5_name = files
+
+# Open the hdf5 file
+hf5 = tables.open_file(hdf5_name, 'r')
+
+# Read in the array of spikes
+spikes_cat = hf5.root.MCMC_switch.categorical_spikes[:]
+
+# Get the laser condition and taste and trial number for this thread (indexed by trial_num above)
+laser_condition = int(trial_num/(spikes_cat.shape[1]*spikes_cat.shape[2]))
+taste_num = int((trial_num - laser_condition*spikes_cat.shape[1]*spikes_cat.shape[2])/spikes_cat.shape[2])
+trial = int((trial_num - laser_condition*spikes_cat.shape[1]*spikes_cat.shape[2]) % spikes_cat.shape[2])
+
+# Change to the correct laser/taste directory
+os.chdir('MCMC_switch/Laser{:d}/Taste{:d}'.format(laser_condition, taste_num))
+
+# Choose the switch function according to the laser condition being used
+switch_functions = {'0': fn.laser_off_trials, '1': fn.laser_early_trials, '2': fn.laser_middle_trials, '3': fn.laser_late_trials}
+
+# Choose the SQLite backend for the model trace
+db = pm.backends.SQLite('trial{:d}.SQLite'.format(trial))
+
+# Get the model and trace after fitting the switching model with MCMC
+model, tr = switch_functions[str(laser_condition)](data[laser_condition, taste_num, trial, :], db)
+
+# Set up things to plot the traceplot for this trial
+fig, axarr = plt.subplots(4, 2)
+axarr = pm.traceplot(tr[250000:], ax = axarr)
+fig.savefig("trial{:d}.png".format(trial))
+plt.close('all')
+
+hf5.close()
+
+
+
+
+
