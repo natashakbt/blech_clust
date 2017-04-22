9a8da4da18f77f10db2e422b790f991f16c1f8ec
narendramukherjee <narendra.mukherjee@gmail.com>
2017-04-22 19:52:18 -0400

    Testing out sampling based on duration of taste epochs

--- additional_analyses/identity_palatability_switch_functions.py
+++ additional_analyses/identity_palatability_switch_functions.py
@@ -17,15 +17,19 @@ def laser_off_trials(data, num_emissions):
 		# Discrete Uniform switch times
 		# Switch from detection to identity firing
 		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		# Duration of identity related firing
+		identity_duration = pm.DiscreteUniform('identity_duration', lower = 20, upper = 100)
 		# Switch from identity to palatability firing
-		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		t2 = pm.Deterministic('t2', t1 + identity_duration)
+		# Duration of palatability related firing
+		palatability_duration = pm.DiscreteUniform('palatability_duration', lower = 30, upper = 120)
 		# Switch from palatability firing to end
-		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 230)
+		t3 = pm.Deterministic('t3', t2 + palatability_duration)
 
 		# Add potentials to keep the switch times from coming too close to each other
-		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
-		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
-		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
+		#t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		#t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
+		#t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
 
 		# Get the actual state numbers based on the switch times
 		states1 = tt.switch(tt.and_(t1 <= np.arange(250), t2 >= np.arange(250)), 1, 0)
