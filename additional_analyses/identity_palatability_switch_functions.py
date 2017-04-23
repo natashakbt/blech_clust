320edf3c9d19383df0cd0beb86452b38918a9f8f
narendramukherjee <narendra.mukherjee@gmail.com>
2017-04-23 14:02:44 -0400

    Fixed marking of states of firing so that switch points don't fall back on each other

--- additional_analyses/identity_palatability_switch_functions.py
+++ additional_analyses/identity_palatability_switch_functions.py
@@ -16,15 +16,11 @@ def laser_off_trials(data, num_emissions):
 
 		# Discrete Uniform switch times
 		# Switch from detection to identity firing
-		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
-		# Duration of identity related firing
-		identity_duration = pm.DiscreteUniform('identity_duration', lower = 20, upper = 100)
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 50)
 		# Switch from identity to palatability firing
-		t2 = pm.Deterministic('t2', t1 + identity_duration)
-		# Duration of palatability related firing
-		palatability_duration = pm.DiscreteUniform('palatability_duration', lower = 30, upper = 120)
+		t2 = pm.DiscreteUniform('t2', lower = t1 + 20, upper = 150)
 		# Switch from palatability firing to end
-		t3 = pm.Deterministic('t3', t2 + palatability_duration)
+		t3 = pm.DiscreteUniform('t3', lower = t2 + 30, upper = 230)
 
 		# Add potentials to keep the switch times from coming too close to each other
 		#t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
@@ -32,17 +28,22 @@ def laser_off_trials(data, num_emissions):
 		#t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
 
 		# Get the actual state numbers based on the switch times
-		states1 = tt.switch(tt.and_(t1 <= np.arange(250), t2 >= np.arange(250)), 1, 0)
-		states2 = tt.switch(tt.and_(t2 <= np.arange(250), t3 >= np.arange(250)), 2, 0)
-		states3 = tt.switch(t3 >= np.arange(250), 0, 3)
-		states = states1 + states2 + states3
+		#states1 = tt.switch(tt.and_(t1 <= np.arange(250), t2 >= np.arange(250)), 1, 0)
+		#states2 = tt.switch(tt.and_(t2 <= np.arange(250), t3 >= np.arange(250)), 2, 0)
+		#states3 = tt.switch(t3 >= np.arange(250), 0, 3)
+		#states = states1 + states2 + states3
+
+		# Get the actual state numbers based on the switch times
+		states1 = tt.switch(t1 >= np.arange(250), 0, 1)
+		states2 = tt.switch(t2 >= np.arange(250), states1, 2)
+		states = tt.switch(t3 >= np.arange(250), states2, 3)
 
 		# Categorical observations
 		obs = pm.Categorical('obs', p = p[states], observed = data[:])
 
 	# Inference button :D
 	with model:
-		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120})
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, progressbar = False)
 
 	# Return the inference!
 	return model, tr[250000:]
@@ -56,29 +57,28 @@ def laser_early_trials(data, num_emissions):
 
 		# Discrete Uniform switch times
 		# Switch from detection to identity firing
-		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 50)
 		# Switch from identity to palatability firing
-		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		t2 = pm.DiscreteUniform('t2', lower = t1 + 20, upper = 140)
 		# Switch from palatability firing to end
-		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 180)
+		t3 = pm.DiscreteUniform('t3', lower = t2 + 30, upper = 180)
 
 		# Add potentials to keep the switch times from coming too close to each other
-		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
-		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
-		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
+		#t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		#t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 30, 0, -np.inf))
+		#t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 50, 0, -np.inf))
 
 		# Get the actual state numbers based on the switch times
-		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
-		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
-		states3 = tt.switch(t3 >= np.arange(200), 0, 3)
-		states = states1 + states2 + states3
+		states1 = tt.switch(t1 >= np.arange(200), 0, 1)
+		states2 = tt.switch(t2 >= np.arange(200), states1, 2)
+		states = tt.switch(t3 >= np.arange(200), states2, 3)
 
 		# Categorical observations
 		obs = pm.Categorical('obs', p = p[states], observed = data[50:])
 
 	# Inference button :D
 	with model:
-		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120})
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, progressbar = False)
 
 	# Return the inference!
 	return model, tr[250000:]	
@@ -92,29 +92,28 @@ def laser_middle_trials(data, num_emissions):
 
 		# Discrete Uniform switch times
 		# Switch from detection to identity firing
-		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 50)
 		# Switch from identity to palatability firing
-		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 100)
+		t2 = pm.DiscreteUniform('t2', lower = t1 + 20, upper = 100)
 		# Switch from palatability firing to end
-		t3 = pm.DiscreteUniform('t3', lower = 90, upper = 180)
+		t3 = pm.DiscreteUniform('t3', lower = t2 + 20, upper = 180)
 
 		# Add potentials to keep the switch times from coming too close to each other
-		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
-		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
-		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
+		#t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		#t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
+		#t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
 
 		# Get the actual state numbers based on the switch times
-		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
-		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
-		states3 = tt.switch(t3 >= np.arange(200), 0, 3)
-		states = states1 + states2 + states3
+		states1 = tt.switch(t1 >= np.arange(200), 0, 1)
+		states2 = tt.switch(t2 >= np.arange(200), states1, 2)
+		states = tt.switch(t3 >= np.arange(200), states2, 3)
 
 		# Categorical observations
 		obs = pm.Categorical('obs', p = p[states], observed = np.append(data[:70], data[120:]))
 
 	# Inference button :D
 	with model:
-		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 60, 't3': 110})
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 60, 't3': 110}, progressbar = False)
 
 	# Return the inference!
 	return model, tr[250000:]	
@@ -128,29 +127,28 @@ def laser_late_trials(data, num_emissions):
 
 		# Discrete Uniform switch times
 		# Switch from detection to identity firing
-		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 40)
+		t1 = pm.DiscreteUniform('t1', lower = 10, upper = 50)
 		# Switch from identity to palatability firing
-		t2 = pm.DiscreteUniform('t2', lower = 30, upper = 130)
+		t2 = pm.DiscreteUniform('t2', lower = t1 + 20, upper = 140)
 		# Switch from palatability firing to end
-		t3 = pm.DiscreteUniform('t3', lower = 100, upper = 180)
+		t3 = pm.DiscreteUniform('t3', lower = t2 + 30, upper = 180)
 
 		# Add potentials to keep the switch times from coming too close to each other
-		t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
-		t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
-		t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
+		#t_pot1 = pm.Potential('t_pot1', tt.switch(t2 - t1 >= 20, 0, -np.inf))
+		#t_pot2 = pm.Potential('t_pot2', tt.switch(t3 - t2 >= 20, 0, -np.inf))
+		#t_pot3 = pm.Potential('t_pot3', tt.switch(t3 - t1 >= 40, 0, -np.inf))
 
 		# Get the actual state numbers based on the switch times
-		states1 = tt.switch(tt.and_(t1 <= np.arange(200), t2 >= np.arange(200)), 1, 0)
-		states2 = tt.switch(tt.and_(t2 <= np.arange(200), t3 >= np.arange(200)), 2, 0)
-		states3 = tt.switch(t3 >= np.arange(200), 0, 3)
-		states = states1 + states2 + states3
+		states1 = tt.switch(t1 >= np.arange(200), 0, 1)
+		states2 = tt.switch(t2 >= np.arange(200), states1, 2)
+		states = tt.switch(t3 >= np.arange(200), states2, 3)
 
 		# Categorical observations
 		obs = pm.Categorical('obs', p = p[states], observed = np.append(data[:140], data[190:]))
 
 	# Inference button :D
 	with model:
-		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120})
+		tr = pm.sample(300000, init = None, step = pm.Metropolis(), njobs = 2, start = {'t1': 20, 't2': 70, 't3': 120}, progressbar = False)
 
 	# Return the inference!
 	return model, tr[250000:]	
