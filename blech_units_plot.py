d6a62d5f341f70a3e07638254adce6bd66c0ba0f
narendramukherjee <narendra.mukherjee@gmail.com>
2017-06-13 19:23:36 -0400

    Fixed time axis label (for scipy paper)

--- blech_units_plot.py
+++ blech_units_plot.py
@@ -36,7 +36,7 @@ for unit in range(len(units)):
 	fig = plt.figure()
 	x = np.arange(waveforms.shape[1]/10)
 	plt.plot(x - 15, waveforms[:, ::10].T, linewidth = 0.01, color = 'red')
-	plt.xlabel('Time')
+	plt.xlabel('Time (30 samples per ms)')
 	plt.ylabel('Voltage (microvolts)')
 	plt.title('Unit %i, total waveforms = %i' % (unit, waveforms.shape[0]) + '\n' + 'Electrode: %i, Single Unit: %i, RSU: %i, FS: %i' % (hf5.root.unit_descriptor[unit]['electrode_number'], hf5.root.unit_descriptor[unit]['single_unit'], hf5.root.unit_descriptor[unit]['regular_spiking'], hf5.root.unit_descriptor[unit]['fast_spiking']))
 	fig.savefig('./unit_waveforms_plots/Unit%i.png' % (unit))
