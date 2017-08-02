909a3d2ffa0f4566ae49dfc4603bfac6ffd8a95f
bradlytstone <bradlytstone@brandeis.edu>
2017-08-02 09:37:53 -0400

    Addressed logic concerns and updated the code to be structurally more simplistic and added more comments for readability.

--- LFP_analysis/Laser_LFP_Parse_Final.py
+++ LFP_analysis/Laser_LFP_Parse_Final.py
@@ -46,23 +46,23 @@ num_trials = hf5.root.spike_trains.dig_in_0.spike_array[:].shape[0]
 for x in range(laser_conditions.shape[0]):
     
     #Create identifier based on laser combination information and create group within "LFP_Lasers" node
-    las_type = 'laser_combos_'+str(int(laser_conditions[x,0]))+'_'+str(int(laser_conditions[x,1]))         
-    hf5.create_group('/LFP_Lasers', str.split(las_type, '/')[-1])
+    las_type = 'laser_combos_d_l_'+str(int(laser_conditions[x,0]))+'_'+str(int(laser_conditions[x,1]))         
+    hf5.create_group('/LFP_Lasers', las_type)
     
     #Loop through taste arrays (dig_in files), identify trial number pertaining to laser condition, and built array with respective LFP data
     for y in range(num_tastes):
-        laser_trial_LFPs = []
         taste = 'taste_' + str(y)
         
-        #Target corresponding LFP array for data indexing
-        lfp_coll = np.mean(lfp_nodes[y][:],axis=(0))
-        start_trial = int(y*(num_trials/num_tastes))
-        trial_group = laser_trials[x,start_trial:int(start_trial+num_trials/num_tastes)]-int(y*num_trials) #Create last of trials to index based on the number of trials per taste (assumes equal trial numbers per tastant)
-        laser_trial_LFPs.append(lfp_coll[trial_group])
-        final_array = np.mean(laser_trial_LFPs,axis=(0))
-        
+        #Collapse data across electrodes to obtain LFPS by tastes and laser conditions
+        lfp_coll = np.mean(lfp_nodes[y][:],axis=0)
+        # Pick the trials of taste y in laser condition x
+        trial_group = np.where((laser_trials[x, :] >= y*num_trials)*(laser_trials[x, :] < (y+1)*num_trials) == True)[0] #Create array of trials to index based on the number of trials per taste and laser combinations (assumes equal trial numbers per taste)
+        # The trials picked above are on the absolute scale of trial numbers (so taste 3, for instance, will have trials 90 to 119 if there were a total of 120 trials). We need to convert this into a 0-29 scale (for 30 trials of each taste) to be able to use these numbers on the LFP arrays
+        trial_group = laser_trials[x, trial_group] - int(y*num_trials)
+        laser_trial_LFPs = lfp_coll[trial_group]
+               
         # Create arrays (based on tastants) to build under corresonding laser condition
-        hf5.create_array('/LFP_Lasers/%s' % las_type, '%s' %taste, np.asarray(final_array))
+        hf5.create_array('/LFP_Lasers/%s' % las_type, '%s' %taste, laser_trial_LFPs)
         hf5.flush()
 
 print("If you want to compress the file to release disk space, run 'blech_hdf5_repack.py' upon completion.")        
