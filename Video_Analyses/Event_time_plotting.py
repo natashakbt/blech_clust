b308216e20b0d458461e1c90753b7dc1b2cc2e69
bradlytstone <bradlytstone@brandeis.edu>
2019-08-07 15:40:32 -0400

    Updated code for behavioral scoring

--- Video_Analyses/Event_time_plotting.py
+++ Video_Analyses/Event_time_plotting.py
@@ -17,7 +17,9 @@ from datetime import datetime
 from matplotlib.patches import Rectangle
 from collections import OrderedDict
 import matplotlib.pyplot as plt
-import string
+from re import search
+import matplotlib as mpl
+import matplotlib.ticker as tkr  
 
 # =============================================================================
 # #Define functions
@@ -30,17 +32,37 @@ def to_xy_stone(some_dictionary):
     x, y = np.array(x).astype(np.int64), np.array(y)
     return x,y
 	
-def plot_events_stone(xval,yval,labs):
+def plot_events_stone(xval,yval,labs,vid_info):
 	#set color palette
 	#colors = plt.get_cmap('winter_r')(np.linspace(0, 1, len(labs)))
 	
-	fig, ax = plt.subplots()
+	fig, ax = plt.subplots(figsize=(24, 16))
 	ax.set_xlim([0, np.max(xval)])
 	ax.set_ylim([-0.5, np.max(yval)+0.5])
 	for i in range(0, len(yval), 2):
-		ax.add_patch(Rectangle((xval[i], yval[i]-0.25), xval[i+1], 0.5, facecolor = 'blue'))
+		ax.add_patch(Rectangle((xval[i], yval[i]-0.25), xval[i+1] - xval[i], 0.5, facecolor = 'blue'))
 	plt.yticks(range(yval.max()+1),labs);
-
+	
+	#Formatting
+	mpl.rcParams['ytick.labelsize'] = 20
+	mpl.rcParams['xtick.labelsize'] = 20
+	plt.xticks(range(0,1200001,60000))
+	ax.get_xaxis().set_major_formatter(tkr.FuncFormatter(numfmt))
+	plt.ylabel('Event',size=21,fontweight="bold")
+	plt.xlabel('Time (minute)',size=21,fontweight="bold")
+	
+	session = search('_(.+)_', vid_info[-15:])[0]
+	plt.title('Animal: %s \nCondition: %s' %(vid_info[:4], session.replace("_","")),size=24,fontweight="bold")  
+	
+	#save output
+	dirname = os.getcwd()
+	fig.savefig(dirname+'/%s_%s' %(vid_info[:4], session.replace("_",""))+ '_Events.png')   
+	plt.close(fig)	
+	
+def numfmt(x, pos): 
+    s = '{}'.format(x / 60000.0)
+    return s
+  
 # =============================================================================
 # Import/Open HDF5 File and variables
 # =============================================================================
@@ -62,15 +84,17 @@ xl_file = pd.ExcelFile(excel_name)
 dfs = {sheet_name: xl_file.parse(sheet_name) 
           for sheet_name in xl_file.sheet_names}
 
-#Create empty list for time difference storing
-time_diff = [];
-for i in len(dfs):  #Flip through all sheets
+#Flip through dataframes to plot
+for i in range(len(dfs)):  #Flip through all sheets
 	#Get sheet name
 	sheet = next(iter(dfs))
 	
 	#Locate start times (00:00:0001) <--Scored this way to allow for pandas processing
 	sheet_df = dfs[sheet].reset_index()
-	start_rows = sheet_df[sheet_df['Time_Start_(min:sec.ms)'] == sheet_df['Time_Start_(min:sec.ms)'].min()]
+	start_rows = sheet_df[sheet_df['Video_File_Name'].str.contains('BS',regex=False)==True]
+	
+	#Establish Video names in file
+	video_names = list(np.array(start_rows['Video_File_Name']))
 	
 	newStart = []; newEnd =[]; timediff= []
 	for n in range(len(sheet_df)):
@@ -88,10 +112,22 @@ for i in len(dfs):  #Flip through all sheets
 	sheet_df['End_time_ms'] = newEnd
 	sheet_df['Time_diff_ms'] = timediff
 	
+	QFreq=[] #For storing frequency of motion arrays	
 	for session in range(len(start_rows)): #Flip through video sessions
-		#set session query
-		sess_query = sheet_df[start_rows.index[session]:start_rows.index[session+1]]
-	
+		if session==0:
+			#set session query
+			sess_query = sheet_df[start_rows.index[session]:start_rows.index[session+1]]
+		else: 
+			#set session query
+			sess_query = sheet_df[start_rows.index[session]:sheet_df.iloc[-1,:][0]+1]
+				
+		#start time
+		timer = int(sess_query['Start_time_ms'][start_rows.index[session]])
+		
+		#reset time
+		sess_query['Start_time_ms'] =  sess_query['Start_time_ms']-timer
+		sess_query['End_time_ms'] =  sess_query['End_time_ms']-timer
+		 
 		#Create categorical values for events and recode
 		sess_query.Event_1 = pd.Categorical(sess_query.Event_1)
 		sess_query['code'] = sess_query.Event_1.cat.codes
@@ -116,85 +152,74 @@ for i in len(dfs):  #Flip through all sheets
 
 		#Plot data
 		x,y = to_xy_stone(beta)
-		plot_events_stone(x,y,np.array([*alpha]))
+		plot_events_stone(x,y,np.array([*alpha]),video_names[session])
 
 # =============================================================================
-# 		newT = []
-# 		for n in range(len(sess_query)):
-# 			raw_t = datetime.strptime(str(sess_query['Time_End'][n]),"%H:%M:%S.%f")-datetime.strptime(str(sess_query['Time_Start_(min:sec.ms)'][n]),"%H:%M:%S.%f")
-# 			newT.append((raw_t.seconds)*1000+raw_t.microseconds/1000)
-# 			
-# 		sess_query['time_diff (ms)'] = newT
-# 		#events = sess_query['Event_1'].unique()
+# 		#Start frequency analyses for movement	
 # =============================================================================
-
-# =============================================================================
-# #Define Variables
-# =============================================================================
-#define color palette
-colors = plt.get_cmap('winter_r')(np.linspace(0, 1, 4))
+		motion_dict = {k: event_dict[k] for k in ('Q1', 'Q2', 'Q3', 'Q4', 'Rearing')}
+		codes = [0,1,2,3,4]
+		motion_dict_coded= dict(zip(codes, list(motion_dict.values())))
+		x,y = to_xy_stone(OrderedDict(sorted(motion_dict_coded.items(), key=lambda x: x[0])))
+ 		
+		#Restrict to start times (eliminates double counts)
+		start_x, start_y = x[::2], y[::2]
+		
+		#Remove rearing from analysis
+		start_xy=list(zip(*[sorted(zip(list(start_y[np.where(start_y != 4)]),list(start_x[np.where(start_y != 4)])), key=lambda x: x[1])]))
+		
+		#Establish where events took place
+		uniquestartValues, startindicesList = np.unique([int(x[0][1]/60000) for x in start_xy], return_index=True)
+		minutesnew = [int(x[0][1]/60000) for x in start_xy]
+		
+		#Create vector over full period
+		events_by_minute = []
+		for min_count in range(0,21):
+			events_by_minute.append(minutesnew.count(min_count))
+		
+		#Store array
+		QFreq.append(np.cumsum(np.array(events_by_minute)))
+		
+		#Plot output
+		fig, ax = plt.subplots(figsize=(12,8))
+		plt.plot(np.cumsum(np.array(events_by_minute)))
+		
+		#Formatting
+		plt.ylabel('Cummulative Frequency',size=18,fontweight="bold")
+		plt.xlabel('Time (minute)',size=18,fontweight="bold")
+		ax.set_xlim([0, 20])
+		ax.set_xticks(range(0,21,2))
+		
+		sessionname = search('_(.+)_', video_names[session][-15:])[0]
+		plt.title('Frequency of Quadrant Movement \nAnimal: %s; Condition: %s' %(start_rows['Video_File_Name'][0][:4],sessionname.replace("_","")),size=18,fontweight="bold")  
+		fig.savefig(dir_name+'/%s_ %s' %(start_rows['Video_File_Name'][0][:4], sessionname.replace("_",""))+ '_QFrequency.png')   
+		plt.close(fig)
+		
+	#Plot QFreq together
+	fig, ax = plt.subplots(figsize=(12,8))
+	plt.plot(QFreq[0],lw=3,color='b',alpha=0.8)
+	plt.plot(QFreq[1],lw=3,color='b',alpha=0.4)
 	
-def generate_data(N = 20):
-    data = [random.randrange(3) for x in range(N)]
-    A = [i for i, x in enumerate(data) if x == 0]
-    B = [i for i, x in enumerate(data) if x == 1]
-    C = [i for i, x in enumerate(data) if x == 2]
-    return A,B,C
-
-def to_xy(*events):
-    x, y = [], []
-    for i,event in enumerate(events):
-        y.extend([i]*len(event))
-        x.extend(event)
-    x, y = np.array(x), np.array(y)
-    return x,y
-
-def event_string(x,y):
-    labels = np.array(list(string.ascii_uppercase))        
-    seq = labels[y[np.argsort(x)]]
-    return seq.tostring()
-
-def plot_events(x,y):
-    labels = np.array(list(string.ascii_uppercase))    
-    plt.hlines(y, x, x+1, lw = 2, color = 'red')
-    plt.ylim(max(y)+0.5, min(y)-0.5)
-    plt.yticks(range(y.max()+1), labels)
-    plt.show()
+	#Get video info
+	vid1 = search('_(.+)_', video_names[0][-15:])[0]; vid2 = search('_(.+)_', video_names[1][-15:])[0]
+	conditions = [vid1.replace("_",""), vid2.replace("_","")]
 	
-A,B,C = generate_data(20)
-x,y = to_xy(A,B,C)
+	#Formatting
+	plt.ylabel('Cummulative Frequency',size=18,fontweight="bold")
+	plt.xlabel('Time (minute)',size=18,fontweight="bold")
+	plt.legend(conditions,prop={'size': 16})
+	ax.set_xlim([0, 20])
+	ax.set_xticks(range(0,21,2))
+	plt.title('Frequency of Quadrant Movement \nAnimal: %s' %(start_rows['Video_File_Name'][0][:4]),size=18,fontweight="bold")  
+	fig.savefig(dir_name+'/%s' %(start_rows['Video_File_Name'][0][:4])+ '_QFrequencyCombined.png')   
+	plt.close(fig)
+			
 
-print(event_string(x,y))
-plot_events(x,y)
 
 
-def plot_events_stone(x,y,labs):
-    labels = labs  
-    plt.hlines(y, x, x+1, lw = 200, color = 'red')
-    plt.ylim(max(y)+0.5, min(y)-0.5)
-    plt.yticks(range(y.max()+1), labels)
-    plt.show()
-	
-	
 
-#plot using hbar
-xx = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
-yy = np.array([0, 0, 1, 0, 0, 0, 1, 1, 2, 2, 0, 2, 0, 2])
 
-labels = np.array(list(string.ascii_uppercase))    
-plt.barh(yy, [1]*len(xx), left=x, color = 'red', edgecolor = 'red', align='center', height=1)
 
-plt.barh(y, [1]*len(x), left=x, color = 'red', edgecolor = 'red', align='center', height=1)
-plt.ylim(max(y)+0.5, min(y)-0.5); 
-plt.yticks(range(y.max()+1), np.array(events)); plt.xlim(0,1000)
 
-fig, ax = plt.subplots()
-ax.set_xlim([0, np.max(x)])
-ax.set_ylim([0, np.max(y)+1])
-for i in range(0, len(y), 2):
-    ax.add_patch(Rectangle((x[i], y[i]), x[i+1], 1, facecolor = "red"))
 
-plt.yticks(range(y.max()+1), np.array([*alpha]));
-  
 
-plt.axvspan(x[0], x[1], facecolor='g', alpha=0.5); plt.axvspan(x[3], x[4], facecolor='g', alpha=0.5);
