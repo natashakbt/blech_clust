821773846cb926c36b03ccfaad73bc696700a6e1
abuzarmahmood <abuzarmahmood@gmail.com>
2020-03-18 18:37:47 -0400

    Changed file output naming

--- umap_spike_scatter_parallel.py
+++ umap_spike_scatter_parallel.py
@@ -28,6 +28,7 @@ def umap_plots(data_dir, electrode_num):
     if os.path.isfile(pca_file):
         
         try:
+            print('Processing Electrode {}'.format(electrode_num))
             pca_waveforms = np.load(data_dir + \
                     '/spike_waveforms/electrode{}/pca_waveforms.npy'.format(electrode_num))
 
@@ -43,16 +44,18 @@ def umap_plots(data_dir, electrode_num):
                     '/spike_times/electrode{}/spike_times.npy'.format(electrode_num))
 
             print('Processing for Electrode {} complete'.format(electrode_num))
+            print('Outputting figures')
 
             for cluster in range(2,cluster_num+1):
 
+                this_clust = clustering_results[cluster-2]
                 fig1, ax1 = plt.subplots()
                 scatter = ax1.scatter(umap_waveforms[:,0],umap_waveforms[:,1],\
-                        c = clustering_results[cluster-2], s = 2, cmap = 'brg')
+                        c = this_clust, s = 2, cmap = 'brg')
                 legend = ax1.legend(*scatter.legend_elements())
                 ax1.add_artist(legend)
                 fig1.savefig(data_dir + \
-                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/cluster{1}_umap.png'.\
+                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/{1}cluster_umap.png'.\
                     format(electrode_num, cluster), 
                     dpi = 300)
                 plt.close(fig1)
@@ -61,7 +64,7 @@ def umap_plots(data_dir, electrode_num):
                 fig2, ax2 = plt.subplots()
                 ax2.hexbin(umap_waveforms[:,0],umap_waveforms[:,1], gridsize = nbins)
                 fig2.savefig(data_dir + \
-                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/cluster{1}_umap_hist.png'.\
+                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/{1}cluster_umap_hist.png'.\
                     format(electrode_num, cluster),
                     dpi = 300)
                 plt.close(fig2)
@@ -70,21 +73,21 @@ def umap_plots(data_dir, electrode_num):
                 nbins = np.min([100,int(umap_waveforms.shape[0]/100)])
                 ax3[0,0].hexbin(spike_times, umap_waveforms[:,0], gridsize = nbins)
                 scatter = ax3[1,0].scatter(spike_times, umap_waveforms[:,0],\
-                        c = clustering_results[cluster-2], s = 2, cmap = 'brg')
+                        c = this_clust, s = 2, cmap = 'brg')
                 legend = ax3[1,0].legend(*scatter.legend_elements())
                 ax3[1,0].add_artist(legend)
 
                 nbins = np.min([100,int(umap_waveforms.shape[0]/100)])
                 ax3[0,1].hexbin(spike_times, umap_waveforms[:,1], gridsize = nbins)
                 scatter = ax3[1,1].scatter(spike_times, umap_waveforms[:,1],\
-                        c = clustering_results[cluster-2], s = 2, cmap = 'brg')
+                        c = this_clust, s = 2, cmap = 'brg')
                 legend = ax3[1,1].legend(*scatter.legend_elements())
                 ax3[1,1].add_artist(legend)
 
                 plt.tight_layout()
 
                 fig3.savefig(data_dir + \
-                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/cluster{1}_umap_timeseries.png'.\
+                    '/Plots/{0}/{1}_clusters_waveforms_ISIs/{1}cluster_umap_timeseries.png'.\
                     format(electrode_num, cluster),
                     dpi = 300)
                 plt.close(fig3)
@@ -93,14 +96,14 @@ def umap_plots(data_dir, electrode_num):
                 nbins = np.min([100,int(pca_waveforms.shape[0]/100)])
                 ax4[0,0].hexbin(spike_times, pca_waveforms[:,0], gridsize = nbins)
                 scatter = ax4[1,0].scatter(spike_times, pca_waveforms[:,0],\
-                        c = clustering_results[cluster-2], s = 2, cmap = 'brg')
+                        c = this_clust, s = 2, cmap = 'brg')
                 legend = ax4[1,0].legend(*scatter.legend_elements())
                 ax4[1,0].add_artist(legend)
 
                 nbins = np.min([100,int(pca_waveforms.shape[0]/100)])
                 ax4[0,1].hexbin(spike_times, pca_waveforms[:,1], gridsize = nbins)
                 scatter = ax4[1,1].scatter(spike_times, pca_waveforms[:,1],\
-                        c = clustering_results[cluster-2], s = 2, cmap = 'brg')
+                        c = this_clust, s = 2, cmap = 'brg')
                 legend = ax4[1,1].legend(*scatter.legend_elements())
                 ax4[1,1].add_artist(legend)
 
@@ -111,6 +114,22 @@ def umap_plots(data_dir, electrode_num):
                     format(electrode_num, cluster),
                     dpi = 300)
                 plt.close(fig4)
+
+                # Plot rasters for each individual cluster
+                max_time = np.max(spike_times)
+                min_y, max_y = np.min(umap_waveforms), np.max(umap_waveforms)
+                for clust_num in np.sort(np.unique(this_clust))[1:]:
+                    relevant_waveforms = umap_waveforms[this_clust == clust_num,0]
+                    relevant_spiketimes = spike_times[this_clust == clust_num]
+                    fig,ax  = plt.subplots()
+                    ax.scatter(relevant_spiketimes,relevant_waveforms, s = 2, alpha = 0.8)
+                    ax.set_xlim(0, max_time)
+                    ax.set_ylim(min_y, max_y)
+                    fig.savefig(data_dir + \
+                        '/Plots/{0}/{1}_clusters_waveforms_ISIs/Cluster{2}_raster.png'.\
+                        format(electrode_num, cluster, clust_num),
+                        dpi = 300)
+                    plt.close(fig)
 
         except:
             # In other words, I'm too lazy to actually debug shit
