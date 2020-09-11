05fb7ea52114d3a066811d3c62f82d4c4726e8f0
abuzarmahmood <abuzarmahmood@gmail.com>
2020-09-11 14:40:13 -0400

    Script to remove all blech_clust related files/folders in a directory

---
+++ blech_clean_slate.py
@@ -0,0 +1,27 @@
+
+# Import stuff!
+import os
+import easygui
+import sys
+import glob
+
+# Get name of directory with the data files
+if sys.argv[1] != '':
+    dir_name = os.path.abspath(sys.argv[1])
+else:
+    dir_name = easygui.diropenbox('Please select data directory')
+
+file_list = os.listdir(dir_name)
+
+# Mark for removal:
+removal_files = ['*.h5','*.params','results.log']
+file_paths = [glob.glob(os.path.join(dir_name, x)) for x in removal_files]
+file_paths = [x[0] for x in file_paths if len(x) > 0]
+removal_dirs = ['clustering_results', 'memory_monitor_clustering',
+        'Plots','spike_times','spike_waveforms']
+dir_paths = [os.path.join(dir_name,x) for x in removal_dirs]
+for this_file in file_paths:
+    os.remove(this_file)
+for this_dir in dir_paths:
+    os.removedirs(this_dir)
+
