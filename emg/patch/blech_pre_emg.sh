403e8b709990b419762ac22e2bac777f70f9f8e8
Abuzar Mahmood <abuzarmahmood@gmail.com>
2022-12-14 18:05:43 -0500

    Don't show waveform plot for EMG

--- emg/patch/blech_pre_emg.sh
+++ emg/patch/blech_pre_emg.sh
@@ -4,5 +4,5 @@ python $BLECH_DIR/blech_clust.py $DIR
 python $BLECH_DIR/emg/patch/adjust_wave_threshold.py $DIR
 bash $BLECH_DIR/blech_clust_jetstream_parallel.sh
 python $BLECH_DIR/emg/patch/select_some_waveforms.py $DIR
-python $BLECH_DIR/blech_post_process.py -d $DIR -f $DIR/*sorted_units.csv
+python $BLECH_DIR/blech_post_process.py -p False -d $DIR -f $DIR/*sorted_units.csv
 bash $BLECH_DIR/blech_clust_post.sh $DIR
