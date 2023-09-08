1a35da339e185f41343c20290772f5e63f65c599
Abuzar Mahmood <abuzarmahmood@gmail.com>
2023-09-08 13:51:14 -0400

    Working verison of plot with functions

--- _experimental/agglomerative_clustering/agg_clust_plot.py
+++ _experimental/agglomerative_clustering/agg_clust_plot.py
@@ -74,6 +74,69 @@ def sort_label_array(label_array):
         for i in range(len(label_array[y])):
             label_array[y][i] = child_map[label_array[y][i]]
     return label_array
+
+def perform_agg_clustering(features, max_clusters = 8):
+    clust_range = np.arange(1, max_clusters+1)
+    ward = AgglomerativeClustering(
+            distance_threshold =0, 
+            n_clusters = None, 
+            linkage="ward").fit(features)
+    linkage = calc_linkage(ward)
+    clust_label_list = [
+            cut_tree(
+                linkage, 
+                n_clusters = this_num
+                ).flatten()
+            for this_num in tqdm(clust_range)
+            ]
+
+    cut_label_array = np.stack(clust_label_list)
+    cut_label_array = sort_label_array(cut_label_array)
+
+    map_dict = {}
+    for i in range(len(clust_range)-1):
+        map_dict[i] = register_labels(
+                cut_label_array[i],
+                cut_label_array[i+1]
+                )
+    return cut_label_array, map_dict, clust_range
+
+def plot_waveform_dendogram(data, cut_label_array, clust_range, plot_n = 1000):
+    slice_mid = data.shape[1]//2
+    fig,ax = plt.subplots(len(clust_range), np.max(clust_range),
+                          figsize = (7,7), sharex = True, sharey = True)
+    for row in range(len(clust_range)):
+        center = int(np.max(clust_range)//2)
+        labels = cut_label_array.T[:,row]
+        unique_labels = np.unique(labels)
+        med_label = int(np.median(unique_labels))
+        ax_inds = unique_labels - med_label + center
+        parent_unique_labels = np.unique(cut_label_array.T[:,row-1])
+        parent_med_label = int(np.median(parent_unique_labels))
+        parent_ax_inds = parent_unique_labels - parent_med_label + center
+        ax[row,0].set_ylabel(f'{clust_range[row]} Clusters')
+        for x in unique_labels:
+            this_dat = data[labels==x] 
+            if len(this_dat) > plot_n:
+                plot_dat = this_dat[np.random.choice(len(this_dat), plot_n)]
+            ax[row, ax_inds[x]].plot(plot_dat.T,
+                color = 'k', alpha = 0.01)
+        if row > 0: 
+            this_map = map_dict[row-1]
+            for key,val in this_map.items():
+                for child in val:
+                    con = ConnectionPatch(
+                            xyA = (slice_mid,0), coordsA = ax[row-1, parent_ax_inds[key]].transData,
+                            xyB = (slice_mid,0), coordsB = ax[row, ax_inds[child]].transData,
+                            arrowstyle = "-|>"
+                            )
+                    fig.add_artist(con)
+    # Remove box round each subplot
+    for ax0 in ax.flatten():
+        ax0.axis('off')
+    fig.savefig(os.path.join('/home/abuzarmahmood/Desktop', 'agg_clust.png'), dpi = 300)
+    plt.close(fig)
+
 ############################################################
 # Load data
 data_dir = '/media/bigdata/projects/neuRecommend/data/sorted/pos'
@@ -97,70 +160,5 @@ plt.show()
 
 ############################################################
 # Cluster
-
-# Pull clusters for a range of cluster numbers
-clust_range = np.arange(2,6)
-ward = AgglomerativeClustering(
-        distance_threshold =0, 
-        n_clusters = None, 
-        linkage="ward").fit(features)
-linkage = calc_linkage(ward)
-clust_label_list = [
-        cut_tree(
-            linkage, 
-            n_clusters = this_num
-            ).flatten()
-        for this_num in tqdm(clust_range)
-        ]
-
-cut_label_array = np.stack(clust_label_list)
-cut_label_array = sort_label_array(cut_label_array)
-
-#plt.imshow(cut_label_array, aspect = 'auto')
-#plt.colorbar()
-#plt.show()
-
-map_dict = {}
-for i in range(len(clust_range)-1):
-    map_dict[i] = register_labels(
-            cut_label_array[i],
-            cut_label_array[i+1]
-            )
-
-plot_n = 1000
-slice_mid = data.shape[1]//2
-fig,ax = plt.subplots(len(clust_range), np.max(clust_range),
-                      figsize = (7,7), sharex = True, sharey = True)
-for row in range(len(clust_range)):
-    #row = 5
-    center = int(np.max(clust_range)//2)
-    labels = cut_label_array.T[:,row]
-    unique_labels = np.unique(labels)
-    med_label = int(np.median(unique_labels))
-    ax_inds = unique_labels - med_label + center
-    parent_unique_labels = np.unique(cut_label_array.T[:,row-1])
-    parent_med_label = int(np.median(parent_unique_labels))
-    parent_ax_inds = parent_unique_labels - parent_med_label + center
-    ax[row,0].set_ylabel(f'{clust_range[row]} Clusters')
-    for x in unique_labels:
-        this_dat = data[labels==x] 
-        if len(this_dat) > plot_n:
-            plot_dat = this_dat[np.random.choice(len(this_dat), plot_n)]
-        ax[row, ax_inds[x]].plot(plot_dat.T,
-            color = 'k', alpha = 0.05)
-    if row > 0: 
-        this_map = map_dict[row-1]
-        for key,val in this_map.items():
-            for child in val:
-                con = ConnectionPatch(
-                        xyA = (slice_mid,0), coordsA = ax[row-1, parent_ax_inds[key]].transData,
-                        xyB = (slice_mid,0), coordsB = ax[row, ax_inds[child]].transData,
-                        arrowstyle = "-|>"
-                        )
-                fig.add_artist(con)
-# Remove box round each subplot
-for ax0 in ax.flatten():
-    ax0.axis('off')
-fig.savefig(os.path.join('/home/abuzarmahmood/Desktop', 'agg_clust.png'), dpi = 300)
-plt.close(fig)
-#plt.show()
+cut_label_array, map_dict, clust_range = perform_agg_clustering(features, max_clusters = 8)
+plot_waveform_dendogram(data, cut_label_array, clust_range, plot_n = 1000)
